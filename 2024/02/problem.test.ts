c14d03ae3b3fe4b0ce8813b031b929b6b5b2d323
simonljus <simon.ljus94@gmail.com>
2024-12-02 10:37:12 +0100

    d02p2solved

--- 2024/02/problem.test.ts
+++ 2024/02/problem.test.ts
@@ -1,10 +1,18 @@
 import { assertEquals } from "@std/assert";
-import { problem1 } from "./problem.ts";
+import { problem1, problem2 } from "./problem.ts";
 const testInputP1 = await Deno.readTextFile("02/test-p1.txt");
 const input = await Deno.readTextFile("02/input.txt");
 Deno.test("p1 test", () => {
-    assertEquals(problem1(testInputP1),2)
-})
+  assertEquals(problem1(testInputP1), 2);
+});
+
+Deno.test("p2 test", () => {
+  assertEquals(problem2(testInputP1), 4);
+});
 Deno.test("p1 Solution", () => {
-    assertEquals(problem1(input),670)
-})
+  assertEquals(problem1(input), 670);
+});
+
+Deno.test("p2 Solution", () => {
+  assertEquals(problem2(input), 700);
+});
--- 2024/02/problem.ts
+++ 2024/02/problem.ts
@@ -1,25 +1,55 @@
-export function problem1(text:string): number{
-    let count =0
-    for(const line of text.split("\n")){
-        let safe = true;
-        const levels = line.split(" ").map(level => Number.parseInt(level.trim()))
-        if(levels.toSorted((a,b) =>a - b).join() !== levels.join() && levels.toSorted((a,b) =>b - a).join() !== levels.join() ){
-            continue
-        }
-        for(const [index,level] of levels.entries()){
-           if(index === 0){
-            continue
-           }
-           const diff = Math.abs(levels[index-1] - level)
-           if(diff <1 || diff > 3){
-                safe = false;
-                break
-           }
-        }
-        if(safe){
-            count +=1
-        }
+export function problem1(text: string): number {
+  let count = 0;
+  for (const line of text.split("\n")) {
+    const levels = line.split(" ").map((level) =>
+      Number.parseInt(level.trim())
+    );
+    if (isSafe(levels)) {
+      count += 1;
     }
-    
-    return count
+  }
+
+  return count;
+}
+
+function isSafe(levels: number[]) {
+  if (
+    levels.toSorted((a, b) => a - b).join() !== levels.join() &&
+    levels.toSorted((a, b) => b - a).join() !== levels.join()
+  ) {
+    return false;
+  }
+  for (const [index, level] of levels.entries()) {
+    if (index === 0) {
+      continue;
+    }
+    const diff = Math.abs(levels[index - 1] - level);
+    if (diff < 1 || diff > 3) {
+      return false;
+    }
+  }
+  return true;
+}
+
+export function problem2(text: string): number {
+  let count = 0;
+  for (const line of text.split("\n")) {
+    const levels = line.split(" ").map((level) =>
+      Number.parseInt(level.trim())
+    );
+    if (isSafe(levels)) {
+      count += 1;
+      continue;
+    }
+    for (let i = 0; i < levels.length; i++) {
+      const copy = levels.slice();
+      copy.splice(i, 1);
+      if (isSafe(copy)) {
+        count += 1;
+        break;
+      }
+    }
+  }
+
+  return count;
 }
