eb627f22fa6f6b07f4a682873f4da358523f1d51
simonljus <simon.ljus94@gmail.com>
2024-12-03 13:56:26 +0100

    formatting

--- 2024/01/problem.test.ts
+++ 2024/01/problem.test.ts
@@ -6,14 +6,13 @@ Deno.test("p1 test", () => {
   assertEquals(problem1(testInputP1), 11);
 });
 
-
 Deno.test("p1 Solution", () => {
   assertEquals(problem1(input), 765748);
 });
 
 Deno.test("p2 test", () => {
-    assertEquals(problem2(testInputP1), 31);
-  });
+  assertEquals(problem2(testInputP1), 31);
+});
 
 Deno.test("p2 Solution", () => {
   assertEquals(problem2(input), 27732508);
--- 2024/01/problem.ts
+++ 2024/01/problem.ts
@@ -1,34 +1,37 @@
-export function problem1(text: string): number{
-const numbers = text.split("\n").map(line => (line.match(/[0-9]+/g) ?? []).map(n => Number.parseInt(n)))
-const lefts: number[] = []
-const rights: number[] = []
-for(const [left,right] of numbers){
-    lefts.push(left)
-    rights.push(right)
+export function problem1(text: string): number {
+  const numbers = text.split("\n").map((line) =>
+    (line.match(/[0-9]+/g) ?? []).map((n) => Number.parseInt(n))
+  );
+  const lefts: number[] = [];
+  const rights: number[] = [];
+  for (const [left, right] of numbers) {
+    lefts.push(left);
+    rights.push(right);
+  }
+  lefts.sort((a, b) => a - b);
+  rights.sort((a, b) => (a - b));
+
+  return lefts.reduce((sum, v, index) => {
+    const diff = Math.abs(rights[index] - v);
+    return sum + diff;
+  }, 0);
 }
-lefts.sort((a,b) => a-b)
-rights.sort((a,b) => (a-b))
 
-return lefts.reduce((sum,v,index) => {
-    const diff = Math.abs(rights[index]- v)
-    return sum + diff
-}, 0 )
+export function problem2(text: string): number {
+  const numbers = text.split("\n").map((line) =>
+    (line.match(/[0-9]+/g) ?? []).map((n) => Number.parseInt(n))
+  );
+  const lefts: number[] = [];
+  const rights: number[] = [];
+  const rightMap = new Map<number, number>();
+  for (const [left, right] of numbers) {
+    lefts.push(left);
+    rights.push(right);
+    rightMap.set(right, (rightMap.get(right) ?? 0) + 1);
+  }
+
+  return lefts.reduce((sum, v) => {
+    const count = rightMap.get(v) ?? 0;
+    return sum + (count * v);
+  }, 0);
 }
-
-
-export function problem2(text: string): number{
-    const numbers = text.split("\n").map(line => (line.match(/[0-9]+/g) ?? []).map(n => Number.parseInt(n)))
-    const lefts: number[] = []
-    const rights: number[] = []
-    const rightMap= new Map<number,number>()
-    for(const [left,right] of numbers){
-        lefts.push(left)
-        rights.push(right)
-        rightMap.set(right,(rightMap.get(right) ?? 0) +1)
-    }
-    
-    return lefts.reduce((sum,v) => {
-        const count =  rightMap.get(v) ?? 0
-        return sum + (count * v)
-    }, 0 )
-    }
