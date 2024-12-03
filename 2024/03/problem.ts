f80b40d76f687c92512a2da07e2bbccf479fa2cc
simonljus <simon.ljus94@gmail.com>
2024-12-03 13:56:44 +0100

    simpler regex

--- 2024/03/problem.ts
+++ 2024/03/problem.ts
@@ -1,29 +1,33 @@
 export function problem1(text: string) {
-  return sumMultiplications(text);
-}
-export function sumMultiplications(text: string) {
   const multiplications = text.match(/mul\(\d+,\d+\)/gm) ?? [];
   return multiplications.reduce((sum, multiplication) => {
-    const factors = (multiplication.match(/\d+/g) ?? []).map((n) =>
-      Number.parseInt(n)
-    );
-    const [factorA, factorB] = factors;
-    const product = factorA * factorB;
-    return product + sum;
+    return multiply(multiplication) + sum;
   }, 0);
 }
+
+export function multiply(text: string) {
+  const factors = (text.match(/\d+/g) ?? []).map((n) => Number.parseInt(n));
+  const [factorA, factorB] = factors;
+  return factorA * factorB;
+}
+
 export function problem2(text: string) {
   const matches = Array.from(
-    `do()${text}do()`.matchAll(/(?<do>do\(\))|(?<dont>don't\(\))/gm),
+    text.matchAll(/(?<mul>mul\(\d+,\d+\))|(?<do>do\(\))|(?<dont>don't\(\))/gm),
   );
-  const { sum } = matches.reduce((obj, match) => {
-    const subtext = text.slice(obj.prevIndex, match.index);
-    const product = obj.op === "dont" ? 0 : problem1(subtext);
-    return {
-      op: match.groups?.do ? "do" : "dont",
-      sum: obj.sum + product,
-      prevIndex: match.index,
-    };
-  }, { op: "do", sum: 0, prevIndex: 0 });
+  const { sum } = matches.reduce((acc, match) => {
+    if (match.groups?.mul) {
+      if (!acc.doMul) {
+        return acc;
+      }
+      const product = multiply(match.groups?.mul);
+      return { ...acc, sum: acc.sum + product };
+    } else if (match.groups?.do) {
+      return { ...acc, doMul: true };
+    } else if (match.groups?.dont) {
+      return { ...acc, doMul: false };
+    }
+    return acc;
+  }, { doMul: true, sum: 0 });
   return sum;
 }
