decaaff6dabd550bf94a70ea66b0358da71fd4b9
simonljus <simon.ljus94@gmail.com>
2024-12-04 09:14:03 +0100

    d4p1 solved

---
+++ 2024/04/problem.test.ts
@@ -0,0 +1,20 @@
+import { assertEquals } from "@std/assert";
+import { problem1, problem2 } from "./problem.ts";
+const testInputP1 = await Deno.readTextFile("04/test-p1.txt");
+const input = await Deno.readTextFile("04/input.txt");
+Deno.test("p1 test", () => {
+  assertEquals(problem1(testInputP1), 18);
+});
+
+Deno.test("p1 Solution", () => {
+  assertEquals(problem1(input), 2543);
+});
+/*
+Deno.test("p2 test", () => {
+  assertEquals(problem2(testInputP2), 48);
+});
+
+Deno.test("p2 Solution", () => {
+  assertEquals(problem2(input), 82733683);
+});
+*/
---
+++ 2024/04/problem.ts
@@ -0,0 +1,55 @@
+export function problem1(text: string):number{
+    const grid = text.split("\n").map(row => row.split(""))
+    let count = 0
+    const word ="XMAS"
+    const firstLetter ="X"
+    const rest =word.slice(1)
+    for(const [y,row] of grid.entries()){
+        for(const [x,letter] of row.entries() ){
+            if(letter !== firstLetter){
+               continue
+            }
+            const ydirs = [-1,0,1]
+            const xdirs = [-1,0,1]
+            for(const dy of ydirs){
+                for(const dx of xdirs){
+                    if(dx === 0 && dy === 0){
+                            continue
+                    }
+                    if(findWord(grid,rest,{x,y},{dx,dy})) {
+                        count+=1
+                    }
+                }
+            }
+        }
+    }
+    return count
+}
+
+export function findWord(grid: string[][],word:string,pos: {x: number, y: number}, dir: {dx: number, dy:number}):boolean{
+    const xCount = grid[0].length
+    const yCount = grid.length
+    for(const [index,char] of word.split("").entries()){
+        const distance = index +1
+        const yPos = (pos.y + dir.dy * distance)
+        const xPos = (pos.x + dir.dx * distance)
+        if(xPos < 0 || xPos >= xCount){
+            return false
+        }
+        if(yPos < 0 || yPos >= yCount){
+            return false
+        }
+        const foundLetter = grid.at(yPos)?.at(xPos)
+        if(!foundLetter){
+            throw new Error(`indexing issue!!! ${xPos},${yPos}`)
+        }
+        if(foundLetter !== char){
+            return false
+        }
+    }
+    return true
+}
+
+export function problem2(text:string){
+    return 0
+}
